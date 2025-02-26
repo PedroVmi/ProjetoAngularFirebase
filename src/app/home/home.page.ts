25deea9405a736588048dbf13c848bf041b9dc93
PedroVmi <160767120+PedroVmi@users.noreply.github.com>
2025-02-26 17:47:19 +0000

    implementação dos cards

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,23 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  pokemon:any = {
-    nome: null,
-    poder: null
-  };
+    url: string = `https://restcountries.com/v3.1/all?fields=name,flags`;
+    is_loading: boolean = false;
+    dados: any = null;
 
-  constructor( 
-    public crudService: CrudService
-  ){ }
-
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
+  constructor( ) {
+    fetch(this.url)
+    .then(dados => dados.json())//converter os dados
+    .then(dados => {
+      console.log(dados);
+      this.dados = dados;
+    })//exibir os dados
+    .catch(_ => {
+      console.log(_);
+    })//tratar possíveis erros
+    .finally(()=>{
+      console.log('Requisição Finalizada!');
+    });//exibir que finalizou
   }
 
 }
